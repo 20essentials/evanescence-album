f7bf5a1fd6515f458176b4a6d0b69165950023c0
DominicDev <s0l0estudi4r@gmail.com>
2025-09-03 17:11:32 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'EVANESCENCE';
+export const nameSongs = [
+	"BRING ME TO LIFE", /*1*/
+	"MY IMMORTAL", /*2*/
+	"LITHIUM", /*3*/
+	"GOING UNDER", /*4*/
+	"EVERYBODY'S FOOL", /*5*/
+	"CALL ME", /*6*/
+	"MISSING", /*7*/
+	"HELLO", /*8*/
+	"WAKE ME UP INSIDE", /*9*/
+	"LOST IN PARADISE", /*10*/
+	"MY HEART IS BROKEN", /*11*/
+	"HAUNTED", /*12*/
+	"BROKEN-SEETHER", /*13*/
+	"MY LAST BREATH", /*14*/
+	"MY IMMORTAL", /*15*/
+	"LOVE EXISTS", /*16*/
+	"GOOD ENOUGH", /*17*/
+	"TOURNIQUET", /*18*/
+	"IMAGINARY", /*19*/
+	"TAKING OVER ME", /*20*/
+]
